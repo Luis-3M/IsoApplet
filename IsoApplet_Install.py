9705fc0f7caa3be1cee120f342ee4661ab7f685e
Luis-3M <luismendesmoreira@hotmail.com>
2017-04-18 16:20:43 +0100

    Build Process script file

---
+++ IsoApplet_Install.py
@@ -0,0 +1,63 @@
+import os, sys, time
+import xml.etree.ElementTree as ET
+
+def menu():
+	print "----------------------------------------------"
+	print "\t ISOAPPLET CAP FILE GENERATOR"
+	print "\t\t by @Luis-3M"
+	print "----------------------------------------------"
+	return
+
+def getPackages():
+	os.system('curl -L https://www.dropbox.com/sh/ztdqovz06z5ylmz/AADdrpO5MqVOae-2E7YsTCQca/src/javacard_2.2.2.tar.gz?dl=1 -O -J')
+	os.system('tar -xf javacard_2.2.2.tar.gz')
+	os.system('rm -rf javacard_2.2.2.tar.gz')
+	os.system('git clone https://github.com/philipWendland/IsoApplet.git')
+	return
+
+def buildCAP():
+	os.chdir(os.path.expanduser('IsoApplet'))
+	os.system('git submodule init && git submodule update')
+	xmlParse()
+	os.system('ant')
+	os.system('mv IsoApplet.cap ~/')
+	os.chdir(os.path.expanduser('~'))
+	os.system('rm -rf IsoApplet/ javacard/')
+	print
+	print '\033[92m'+str(os.path.abspath('IsoApplet.cap'))+'\033[0m'
+	return
+
+def xmlParse():
+	tree = ET.parse('build.xml')
+	root = tree.getroot()
+	for javacard in root.iter('javacard'):
+		javacard.set('jckit','~/javacard/javacard_2_2_2')
+	tree.write('build.xml')
+	return
+
+def main():
+	start = time.time()
+	os.system('clear')
+	os.chdir(os.path.expanduser('~'))
+	menu()
+	try:
+		opt = raw_input("Do you have '\033[4m'ANT'\033[0m', '\033[4m'GIT'\033[0m' and '\033[4m'GGPRO'\033[0m' installed? ")
+		if opt.lower() == "yes":
+			getPackages()
+			buildCAP()
+			print
+		elif opt.lower() == "no":
+			print "Please install them first.."
+		else:
+			print "Really? Don't even think about it.. Yes or No mate."
+	except KeyboardInterrupt:
+		sys.exit(0)
+	print
+	end = time.time()
+	execTime = round(end-start,4)
+	
+	print "Program executed in "+str(execTime)+" seconds"
+	return
+
+if __name__ == "__main__":
+	main()
